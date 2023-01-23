f7f354f15846227ce9a87d1c9b7294e0da65a302
igalvaoq <igor.quaresma@uni-muenster.de>
2023-01-23 19:43:25 +0100

    added quick use leaflet-geoserver-request links

--- StationsWuppertal.js
+++ StationsWuppertal.js
@@ -1,16 +1,17 @@
-var map = L.map('map').setView([51.266,7.175], 13);   //Breitengrad und Längengrad von Wuppertal, eigentliche Breitengrad und Längengrad sind [51.2562 , 7.1508]
+var map = L.map("map").setView([51.266, 7.175], 13); //Breitengrad und Längengrad von Wuppertal, eigentliche Breitengrad und Längengrad sind [51.2562 , 7.1508]
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
- maxZoom: 19,
- attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
- }).addTo(map);
+var osm = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+});
 
+osm.addTo(map);
 
-    
-    L.marker([51.266,7.175], 13).addTo(map)
-        .bindPopup('Wuppertal')
-        .openPopup();
-    //L.marker(pos).addTo(map)
-      //  .bindPopup('You are here.')
-        //.openPopup();
-    
+L.marker([51.266, 7.175], 13).addTo(map).bindPopup("Wuppertal").openPopup();
+//L.marker(pos).addTo(map)
+//  .bindPopup('You are here.')
+//.openPopup();
+
+// wms request
+var wmsLayer = L.geo;
